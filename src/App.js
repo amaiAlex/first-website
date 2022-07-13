bdb1f202c0b8851837fbb4c71ae2cf69b3794e29
amialex <dlakpi@ukr.net>
2022-07-14 00:35:54 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Fragment } from 'react';
+import './App.css';
+import FirstPage from './components/FirstPage';
+
+function App() {
+  return (
+    <Fragment>
+      <FirstPage />
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/FirstPage.jsx
@@ -0,0 +1,9 @@
+function FirstPage() {
+  return (
+    <div className="App">
+      <h1>Hi Lesha</h1>
+    </div>
+  );
+}
+
+export default FirstPage;
