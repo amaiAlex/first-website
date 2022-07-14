37bf134e22263159040627ab11198147282768c2
amialex <dlakpi@ukr.net>
2022-07-15 01:08:00 +0300

    Added self info and webpages links

--- src/components/FirstBlock.jsx
+++ src/components/FirstBlock.jsx
@@ -1,12 +1,31 @@
 function FirstBlock() {
   return (
     <div className="app">
-      <h1>Hi Guys</h1>
-      <p>This is a list of Lesha's cool attributes</p>
-      <ul>
-        <li>Lesha's penis - exists</li>
-        <li>Lesha's Brain - none</li>
-      </ul>
+      <h1 className="center"><strong>Alexey Lastowetskiy</strong></h1>
+      <h4 className="center">Innovator, Spiritual Leader, Biggest Penis in the World</h4>
+     <div className="columns">
+       <div className="column">
+        <p><strong>ABOUT</strong></p>
+        <p>Alexei has recently finished his Computer Science degree with Distinction. On the day of his graduation, the Dean cried upon realizing he would never see Alexei again, as the latter one had decided to travel the world and share his wisdom, following the footsteps of his mentor and close friend – Buddha.</p>
+        <p>The journey was spiritually enlightening and Alexei became the first person ever to achieve a state of Super-Nirvana. Along his way he never spoke a word and remained abstinent from human contact. Nevertheless, by his sheer presence, he trained numerous disciples and fathered 48 children.</p>
+        <div className="columns">
+          <a href="https://www.telegram.com">
+           <img src="https://play-lh.googleusercontent.com/ZU9cSsyIJZo6Oy7HTHiEPwZg0m2Crep-d5ZrfajqtsH-qgUXSqKpNA2FpPDTn-7qA5Q" alt="Telegram" width="50" height="50" />
+          </a>
+          <a href="https://www.instagram.com/ne_smotri.na.menya">
+            <img src="https://englishlib.org/dictionary/img/wlibrary/i/602049775693c8.52478975.jpg" alt="Instagram" width="50" height="50" />
+          </a>
+        </div>
+       </div>
+       <div className="column">
+        <p><strong>ACHIEVEMENTS</strong></p>
+        <p>Longest distance ran: 904 miles</p>
+        <p>Deadlift world record holder: 545 kg</p>
+        <p> Longest sex session: 102 hours</p>
+        <p> Noble Prizes in Peace, Physics, and Chemistry</p>
+        <p> Youngest chess Grand Master at the age of 9 months</p>
+       </div>
+     </div>
     </div>
   );
 }
