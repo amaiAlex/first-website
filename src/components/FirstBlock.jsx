d70b524b71e9eaebcd162408c5b542207176f93e
amialex <dlakpi@ukr.net>
2022-07-17 00:00:15 +0300

    Added head logo and self info

--- src/components/FirstBlock.jsx
+++ src/components/FirstBlock.jsx
@@ -1,16 +1,19 @@
 function FirstBlock() {
   return (
     <div className="app">
-      <h1 className="center"><strong>Alexey Lastowetskiy</strong></h1>
+      <h1 className="center"><strong>Alexey Lastovetskyi</strong></h1>
       <h4 className="center">Innovator, Spiritual Leader, Biggest Penis in the World</h4>
      <div className="columns">
        <div className="column">
         <p><strong>ABOUT</strong></p>
         <p>Alexei has recently finished his Computer Science degree with Distinction. On the day of his graduation, the Dean cried upon realizing he would never see Alexei again, as the latter one had decided to travel the world and share his wisdom, following the footsteps of his mentor and close friend – Buddha.</p>
         <p>The journey was spiritually enlightening and Alexei became the first person ever to achieve a state of Super-Nirvana. Along his way he never spoke a word and remained abstinent from human contact. Nevertheless, by his sheer presence, he trained numerous disciples and fathered 48 children.</p>
+        <p><strong>GET IN TOUCH</strong></p>
+        <p>Alexei is always looking to share his infinite wisdom and give back some of his truly divine gifts. He has shared his kidneys 928 times, but they always grow back. He has donated over $2 trillion to charities and noble causes using the fortune he amassed over his gambling career. Don’t hesitate to ask, and your prayers will be answered – you just need to address the appropriate deity.</p>
         <div className="columns">
           <a href="https://www.telegram.com">
-           <img src="https://avatanplus.com/files/resources/original/5e73948500afa170f37c278b.png" alt="Telegram" width="50" height="50" />
+           <img src="https://avatanplus.com/files/resources/original/5e73948500afa170f37c278b.png
+           " alt="Telegram" width="50" height="50" />
           </a>
           <a href="https://www.instagram.com/ne_smotri.na.menya">
             <img src="https://pngicon.ru/file/uploads/instagram.png" alt="Instagram" width="50" height="50" />
@@ -24,6 +27,20 @@ function FirstBlock() {
         <p> Longest sex session: 102 hours</p>
         <p> Noble Prizes in Peace, Physics, and Chemistry</p>
         <p> Youngest chess Grand Master at the age of 9 months</p>
+        <p><strong>BOOKS:</strong></p>
+        <div className="nomargin">
+          <p><i>Lessons of life</i></p>
+        </div>
+        <p>April 2020</p>
+        <div className="nomargin">
+          <p><i>Genius in the flesh</i></p>
+        </div>
+        <p>December 2019</p>
+        <div className="nomargin">
+         <p><i>How to grow your penis in the sun</i></p>
+        </div>
+        <p>November 2017</p>
+        
        </div>
      </div>
     </div>
