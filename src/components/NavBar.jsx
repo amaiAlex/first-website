68dfb459c1b9a55be66cfd154b317a480c3ac478
StanHus <86333618+StanHus@users.noreply.github.com>
2022-07-17 09:19:30 +0100

    modified favicon

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,7 +5,10 @@ export default function NavBar() {
     <Navbar bg="dark" variant="dark" expand="lg">
       <Container>
         {/* If you want to add a new page, just put any new href and it will be a page in your site, just like the line below */}
-        <Navbar.Brand href="/">Lesha's Website</Navbar.Brand>
+        <Navbar.Brand href="/">
+          <img src="/favicon.ico" alt="favicon" className="inline-image" />
+          Lesha's Website
+        </Navbar.Brand>
       </Container>
     </Navbar>
   );
