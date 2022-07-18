e01c8c1b3d2b1a3d49c8c73cf6db09d871a58e6a
amialex <dlakpi@ukr.net>
2022-07-19 01:40:51 +0300

    Created latest news, blog

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,15 +1,10 @@
+import news from "./news";
 function Blog() {
     return (
       <div>
-        <h1>Doctors deliver crushing news</h1>
-    <img src="https://alexeilastovetski.files.wordpress.com/2021/06/doctors.jpg" alt="logo" height="100px" />
-    <p>After countless consultations, Royal Medical Academy experts concluded that penis reduction surgery is not an option for Alexei.
-“I am honestly devastated by these news” – Alexei said in an interview with CNBC, – “45 cm is just too much, so I hoped that a medical intervention would help me, but alas – I remain burdened with this glorious gift”.
-Ever since he developed an Enormous Penis Syndrome (EPS) at the age of 10, his sex life has been challenging to say the least. Some of the girls faint in the process, but the biggest issue is that it takes him at least 6 hours to finish, which is just inconvenient as far as even basic scheduling is concerned.
-Before we concluded our meeting, Alexei sighed and remarqued that he wishes he was just a mere man instead of this sub-divine gift to mankind, but it seems you have to play with the cards you are dealt with.
-Once again, the world holds its breath in anticipation what Alexei does next. CNBC will continue keeping you updated, but for now we must be thankful for His contributions to this date.
-Mon, 28th of June, 2021 Yaroslav Summergeese, CNBC</p>
-
+    <h2>{news[0].title}</h2>
+    <p>{news[0].text}</p>
+    <img src={news[0].image} alt="doctors" />
       </div>
     );
   }
