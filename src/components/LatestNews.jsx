e4348fdfe4cb1e8c7b2ffec5bcaf95bc0e09ed22
amialex <dlakpi@ukr.net>
2022-07-19 01:41:35 +0300

    Updated latest news

--- src/components/LatestNews.jsx
+++ src/components/LatestNews.jsx
@@ -1,7 +1,13 @@
+import news from "./news";
+
 function LatestNews() {
     return (
       <div>
     <h1>Latest-News</h1>
+    <h2>{news[0].title}</h2>
+    <p>{news[0].text.split(' ').slice(0, 30).join(' ')}</p>
+    <img src={news[0].image} alt="doctors" />
+    <a href={news[0].url}>Continue reading</a>
       </div>
     );
   }
