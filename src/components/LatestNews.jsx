b93766e2abb903da9be2e4035baeffea9f68648b
amialex <dlakpi@ukr.net>
2022-07-23 01:44:35 +0300

    edited latest-news view

--- src/components/LatestNews.jsx
+++ src/components/LatestNews.jsx
@@ -4,10 +4,17 @@ function LatestNews() {
   return (
     <div>
       <h1>Latest-News</h1>
-      <h2>{news[0].title}</h2>
-      <p>{news[0].text.split(' ').slice(0, 30).join(' ')}</p>
-      <img src={news[0].image} alt="doctors" />
-      <a href={news[0].url}>Continue reading</a>
+      <div className="columns-news">
+        <img className="column-news-image" src={news[0].image} alt="doctors" />
+        <div className="column-news">
+          <h2>{news[0].title}</h2>
+          <p>{news[0].text.split(' ').slice(0, 100).join(' ')}</p>
+          <a className="link-button" href={news[0].url}>
+            <h4>Continue reading</h4>
+            <span className="arrow"></span>
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
